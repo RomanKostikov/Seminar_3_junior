a206b4fed210302b87f1c79c5afe008d72d4921f
Roman <romankos1993@gmail.com>
2022-10-07 20:39:41 +0300

    task_2 completed

---
+++ task_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+Console.Clear();
+Console.Write("Введите номер четверти: ");
+int number = int.Parse(Console.ReadLine()!);
+if (number == 1)
+{
+    Console.WriteLine("x>0, y>0");
+}
+if (number == 2)
+{
+    Console.WriteLine("x<0, y>0");
+}
+if (number == 3)
+{
+    Console.WriteLine("x<0, y<0");
+}
+if (number == 4)
+{
+    Console.WriteLine("x>0, y<0");
+}
