a5a84434da436fbd8e9d0ad74212fd8738091d1a
Roman <romankos1993@gmail.com>
2022-10-07 21:42:27 +0300

    task_4 completed

---
+++ task_4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+Console.Clear();
+Console.Write("Введите число N: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int result = 1;
+int pow = 1;
+
+while (result <= number)
+{
+    pow = result * result;
+    Console.WriteLine($"{pow}");
+    result++;
+}
